f921a78fa98d53ec18ff88a9504c4fa4e769e017
Chris Greening <chris@cmgresearch.com>
2025-08-31 19:57:10 +0100

    Adds code coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { coverageV8 } from '@vitest/coverage-v8'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './coverage'
+    }
+  }
 })
