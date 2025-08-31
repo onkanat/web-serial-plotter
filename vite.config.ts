4f2a2fd2fda5b6484ee6c5c53c6f0a0887a40075
Chris Greening <chris@cmgresearch.com>
2025-08-31 20:05:41 +0100

    Fix linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { coverageV8 } from '@vitest/coverage-v8'
 
 // https://vite.dev/config/
 export default defineConfig({
